69a814b978510c0dde247a6e881a083b380cca08
dauduchieu <dauduchieu.ddh775@gmail.com>
2026-01-19 21:34:01 +0700

    v2

---
+++ build_index.py
@@ -0,0 +1,33 @@
+import argparse, json, os
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader("templates"))
+DIST = "main"
+
+def build_site(template_name: str, slug: str, context: dict):
+    html = env.get_template(template_name).render(context)
+
+    out_dir = os.path.join(DIST, slug)
+    os.makedirs(out_dir, exist_ok=True)
+
+    with open(os.path.join(out_dir, "index.html"), "w", encoding="utf-8") as f:
+        f.write(html)
+        
+parser = argparse.ArgumentParser()
+parser.add_argument("--posts", required=True)
+parser.add_argument("--site", required=True)
+parser.add_argument("--year", type=int, required=True)
+
+args = parser.parse_args()
+posts = json.loads(args.posts)
+site = json.loads(args.site)
+
+print("Number of posts:", len(posts))
+print("Year:", args.year)
+print("Site title:", site["title"])
+
+build_site("index.html", "", {
+    "posts": posts,
+    "site": site,
+    "now": {"year": args.year}
+})
---
+++ build_post.py
@@ -0,0 +1,36 @@
+import argparse, json, os
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader("templates"))
+DIST = "main"
+
+def build_site(template_name: str, slug: str, context: dict):
+    html = env.get_template(template_name).render(context)
+
+    out_dir = os.path.join(DIST, slug)
+    os.makedirs(out_dir, exist_ok=True)
+
+    with open(os.path.join(out_dir, "index.html"), "w", encoding="utf-8") as f:
+        f.write(html)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--slug", required=True)
+parser.add_argument("--post", required=True)
+parser.add_argument("--site", required=True)
+parser.add_argument("--year", type=int, required=True)
+
+args = parser.parse_args()
+
+post = json.loads(args.post)
+site = json.loads(args.site)
+
+print("Slug:", args.slug)
+print("Title:", post["title"])
+print("Site title:", site["title"])
+print("Year:", args.year)
+
+build_site("post.html", args.slug, {
+    "post": post,
+    "site": site,
+    "now": {"year": args.year}
+})
